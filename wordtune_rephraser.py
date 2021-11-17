acc7d27c4f047e0aaeed9ca9328d8d3bfa232839
Hitesh Shekhat <shekhat.hitesh@gmail.com>
2021-11-17 23:31:05 +0530

    Error of selecting paragraph in editor resolved using javascript

--- wordtune_rephraser.py
+++ wordtune_rephraser.py
@@ -12,9 +12,9 @@ from time import sleep
 
 load_dotenv()
 
-my_email = os.getenv('EMAIL')
-my_password = os.getenv('PASSWORD')
-chrome_driver_path = os.getenv('CHROME_DRIVER_PATH')
+my_email = os.getenv("EMAIL")
+my_password = os.getenv("PASSWORD")
+chrome_driver_path = os.getenv("CHROME_DRIVER_PATH")
 
 
 def rephraser(docs_url):
@@ -22,78 +22,84 @@ def rephraser(docs_url):
     driver.maximize_window()
     driver.get(docs_url)
     action = ActionChains(driver)
-    action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
-    action.key_down(Keys.CONTROL).send_keys('c').key_up(Keys.CONTROL).perform()
+    action.key_down(Keys.CONTROL).send_keys("a").key_up(Keys.CONTROL).perform()
+    action.key_down(Keys.CONTROL).send_keys("c").key_up(Keys.CONTROL).perform()
     sleep(5)
     driver.execute_script("window.open('');")
     driver.switch_to.window(driver.window_handles[1])
-    driver.get('https://app.wordtune.com/login')
-    email = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//input[@name="email"]')))
+    driver.get("https://app.wordtune.com/login")
+    email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@name="email"]')))
     email.send_keys(my_email)
-    password = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//input[@name="password"]')))
+    password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@name="password"]')))
     password.send_keys(my_password)
-    login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, '//button[@data-testid="login_button"]')))
+    login_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//button[@data-testid="login_button"]'))
+    )
     login_button.click()
     sleep(5)
-    driver.get('https://app.wordtune.com/editor')
+    driver.get("https://app.wordtune.com/editor")
     try:
-        welcome_window = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.XPATH, '//body/div[3]/div[3]/div/div/div/div[1]/div[4]')))
+        welcome_window = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//body/div[3]/div[3]/div/div/div/div[1]/div[4]"))
+        )
     except TimeoutException as e:
-        welcome_window = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.XPATH, '//body/div[4]/div[3]/div/div/div/div[1]/div[4]')))
+        welcome_window = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//body/div[4]/div[3]/div/div/div/div[1]/div[4]"))
+        )
     welcome_window.click()
-    editor = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, '//div[@id="editorContentEditable"]')))
-    editor.send_keys(Keys.CONTROL, 'v')
+    editor = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//div[@id="editorContentEditable"]'))
+    )
+    editor.send_keys(Keys.CONTROL, "v")
     sleep(2)
-    # child_elements_p = editor.find_elements_by_xpath('//p/span')
-    # child_elements_h2 = editor.find_elements_by_xpath('//h2/span')
-    # child_elements_h3 = editor.find_elements_by_xpath('//h3/span')
-    child_elements_p = editor.find_elements_by_tag_name('p')
-    child_elements_h2 = editor.find_elements_by_tag_name('h2')
-    child_elements_h3 = editor.find_elements_by_tag_name('h3')
+    child_elements_p = editor.find_elements_by_xpath("//p/span")
+    child_elements_h2 = editor.find_elements_by_xpath("//h2/span")
+    child_elements_h3 = editor.find_elements_by_xpath("//h3/span")
+    # child_elements_p = editor.find_elements_by_tag_name('p')
+    # child_elements_h2 = editor.find_elements_by_tag_name('h2')
+    # child_elements_h3 = editor.find_elements_by_tag_name('h3')
     child_elements = child_elements_p + child_elements_h3 + child_elements_h2
 
-
+    js = """var sel = window.getSelection ? window.getSelection() : document.selection;
+        if (sel) {
+            if (sel.removeAllRanges) {
+                sel.removeAllRanges();
+            } else if (sel.empty) {
+                sel.empty();
+            }
+        }"""
     for child in child_elements:
+        driver.execute_script('arguments[0].scrollIntoView({block: "center", inline: "center"})', child)
         driver.execute_script(
-            'arguments[0].scrollIntoView({block: "center", inline: "center"})', child)
-        a = ActionChains(driver)
-        a.move_to_element(child)
-        driver.switch_to_active_element
-        sleep(1)
-        for i in range(3):
-            a.click()
-        a.perform()
-        rewrite_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.XPATH, '//button[@data-testid="editorToolbarButtonRewrite"]')))
+            "if (document.body.createTextRange) {const range = document.body.createTextRange();range.moveToElementText(arguments[0]);range.select();} else if (window.getSelection) {const selection = window.getSelection();const range = document.createRange();range.selectNodeContents(arguments[0]);selection.removeAllRanges();selection.addRange(range);}",
+            child,
+        )
+        rewrite_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//button[@data-testid="editorToolbarButtonRewrite"]'))
+        )
         rewrite_button.click()
         # a.key_down(Keys.CONTROL).send_keys('d').key_up(Keys.CONTROL).perform()
         for i in range(100000):
             try:
-                first_div = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-                    (By.XPATH, '//div[@data-testid="RewriteBox"]/div[1]/div[1]')))
+                first_div = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, '//div[@data-testid="RewriteBox"]/div[1]/div[1]'))
+                )
                 first_div.click()
                 sleep(1)
             except TimeoutException as e:
                 break
-        driver.execute_script(js,child)
+        driver.execute_script(js, child)
 
-    editor.send_keys(Keys.CONTROL, 'a')
-    editor.send_keys(Keys.CONTROL, 'c')
+    editor.send_keys(Keys.CONTROL, "a")
+    editor.send_keys(Keys.CONTROL, "c")
     sleep(1)
     driver.switch_to.window(driver.window_handles[0])
     sleep(3)
     a = ActionChains(driver)
-    a.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
-    a.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+    a.key_down(Keys.CONTROL).send_keys("a").key_up(Keys.CONTROL).perform()
+    a.key_down(Keys.CONTROL).send_keys("v").key_up(Keys.CONTROL).perform()
     sleep(20)
 
 
 docs_url = "https://docs.google.com/document/d/1fqQ6U9f5wXMX0qL3p4OjEor7D7dtZtB0xXCM_ouP5NQ/edit#"
 w = rephraser(docs_url)
-
